f196c08870816d8b506980a71daae724b72f6b78
Altobert <alberto.sanmartin@usach.cl>
2023-12-16 01:03:51 -0300

    correcciones

---
+++ config-server/config-server/src/main/java/cl/config/server/ConfigServerApplication.java
@@ -0,0 +1,15 @@
+package cl.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer // Habilita el servidor de configuración
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
