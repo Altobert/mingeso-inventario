67694c2b155060b85b880f51674390ef3172840c
Altobert <alberto.sanmartin@usach.cl>
2023-12-19 20:51:52 -0300

    fuentes persistencia

--- ms-ingreso/src/main/java/cl/mingeso/msingreso/entity/Prestamo.java
+++ ms-ingreso/src/main/java/cl/mingeso/msingreso/entity/Prestamo.java
@@ -1,6 +1,8 @@
 package cl.mingeso.msingreso.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
@@ -21,8 +23,8 @@ import lombok.Setter;
 public class Prestamo {
 
     @Id
-    @NotNull
-    Integer idPrestamo;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
     Integer idProfesor;
     Integer idProyector;
     String fechaPrestamo;
